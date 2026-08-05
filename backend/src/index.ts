9be9d0dcc502239b104f48c924a1fcf7c3306513
hrdpaliwal93 <hardikpaliwal2015@gmail.com>
2026-08-05 15:35:26 +0530

    feat:  user signup with input validation and username uniqueness check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,34 +10,46 @@ app.use(express.json())
 
 
 app.post('/signup', async (req, res) => {
+  //validating user inputs
   const inputUser = z.object({
-    username :z.string,
-    email :  z.email,
-    password : z.mi
+    username: z.string(),
+    email: z.email(),
+    password: z.string().min(3).max(8)
   })
-  const { email, username, password } = req.body
-  try {
-    const user = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password
-      }
-    })
+  const { success, data, error } = inputUser.safeParse(req.body)
+  if (!success) { res.json({ message: "invalid input format", error: error.message }); return; }
 
-    res.json({
-      message:"signup successful",
-      success:true
-    })
+
+  const { username, email, password } = req.body
+  try {
+        //checking if the username already exixts or not!
+        const user = await prisma.user.findFirst({where:{username:username}})
+
+        if(user) {res.json({message:"username already exists"});return ;}
+
+        //getting user password and hashing it before saving to db !
+        const hashedpassword = b.hash(password, 10)
+        await prisma.user.create({
+          data: {
+            username,
+            email,
+            password: hashedpassword
+          }
+        })
+
+        res.json({
+          message: "signup successful",
+          success: true
+        })
 
   } catch (e) {
     console.error(e)
-   
+
   }
 })
 
 
-app.post('/login', (req,res)=>{
+app.post('/login', (req, res) => {
 
 })
 app.listen(8000)
